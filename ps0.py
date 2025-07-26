8584b9a3663f0e08d94ec78ea09cbb4947dd6405
Wissem <asqr43641@gmail.com>
2025-07-26 18:58:30 +0100

    Day 1

---
+++ ps0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 26 18:18:16 2025
+
+@author: wissem
+"""
+import numpy
+x = 5
+y = 8
+z = x + y
+a = numpy.log2(z)
+print(a)
